aa76efb2f6343c7f1364ec4297b7efb39a1ccf71
Aiman Azmie <aimanaaw@gmail.com>
2019-11-14 10:16:04 -0500

    Adding an about the app feature complete with route

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,8 +3,6 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    # @lines = Order.find(params[:id]).line_items
-    # raise @order.line_items.product[0].inspect
   end
 
   def create
